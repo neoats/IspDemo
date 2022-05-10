3caa2ad3edb45d426487f569c9a66fbc4bf0b62d
fuat atessacan <fuatatessacan@gmail.com>
2022-05-11 02:12:30 +0300

    1

---
+++ KütüphaneDemo/DijitalFilm/DijitalFilm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KütüphaneDemo.DijitalFilm
+{
+    class DijitalFilm :IKiralananDVD
+    {
+
+        public string KutuphaneId { get; set; }
+
+        public string UrunAdı { get; set; }
+
+        public DateTime KiraTarih { get; set; }
+        public string Kiralayan { get; set; }
+        public int KalanGun { get; set; } = 14;
+        public List<string> Actors { get; set; }
+        public int CalmaSuresi { get; set; }
+
+        public DateTime BitisTarih()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KontrolIn()
+        {
+            Kiralayan = "";
+        }
+
+        public void KontrolOut(string kiralayan)
+        {
+            Kiralayan = kiralayan;
+            KiraTarih = DateTime.Now;
+        }
+    }
+}
