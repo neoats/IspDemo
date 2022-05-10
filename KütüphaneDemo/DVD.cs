fce0df4ec9a5820d8b25ad479edaff5a5fe1970c
fuat atessacan <fuatatessacan@gmail.com>
2022-05-10 15:27:15 +0300

    2

---
+++ KütüphaneDemo/DVD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KütüphaneDemo
+{
+    class DVD : IKutuphaneItem
+    {
+        public string Yazar { get; set; }
+        public string UrunAdı { get; set; }
+
+        public DateTime KiraTarih { get; set; }
+        public string Kiralayan { get; set; }
+        public int KalanGun { get ; set; }
+        public string KutuphaneId { get; set; }
+        public int Sayfa { get ; set ; }
+
+        public DateTime BitisTarih()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KontrolIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KontrolOut(string kiralayan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KütüphaneDemo/IKutuphaneItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KütüphaneDemo
+{
+    interface IKutuphaneItem
+    {
+        string KutuphaneId { get; set; }
+        string UrunAdı { get; set; }
+        string  Yazar { get; set; }
+        DateTime KiraTarih { get; set; }
+        string Kiralayan { get; set; }
+        int KalanGun { get; set; }
+        int Sayfa { get; set; }
+        void KontrolIn();
+        void KontrolOut(string kiralayan);
+        DateTime BitisTarih();
+    }
+}
